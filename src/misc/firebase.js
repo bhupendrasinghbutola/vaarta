0ee40955b0f001c53d4a9c5b40fb71d75937d9e8
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-06-09 21:49:40 +0530

    initialised Firebase app

---
+++ src/misc/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxnrHYF5W4bZYih7QTvFiVRFuz7eoE9_8",
+  authDomain: "chat-web-app-7748e.firebaseapp.com",
+  projectId: "chat-web-app-7748e",
+  storageBucket: "chat-web-app-7748e.appspot.com",
+  messagingSenderId: "602032636013",
+  appId: "1:602032636013:web:764dccd1b188f7c8017f64"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
