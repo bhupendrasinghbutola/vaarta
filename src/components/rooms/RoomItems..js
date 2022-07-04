9444798576de9f8acfb7c6b79348c1f06dcf8282
bhupendrasinghbutola <bhuppiammubutola@gmail.com>
2022-07-05 02:45:41 +0530

    displayed message inside chat page

--- src/components/rooms/RoomItems..js
+++ src/components/rooms/RoomItems..js
@@ -9,10 +9,12 @@ const RoomItem = ({room}) => {
     <div>
         <div className="d-flex justify-content-between align-items-center">
 <h3 className="text-disappear">{name}</h3>
-<TimeAgo datetime={lastMessage  ? new Date(lastMessage.createdAt): new Date(createdAt)} 
+<TimeAgo 
+datetime={
+  lastMessage  ? new Date(lastMessage.createdAt): new Date(createdAt)
+} 
 className="font-normal text-black-45"/>
-{/* <span>xx </span> */}
-        </div>
+</div>
         
         
         <div  className="d-flex align-items-center text-black-70">
